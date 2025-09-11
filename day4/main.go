c949daf03a635b639237b143b435f9b0debce577
PP laptop <petervsjim@gmail.com>
2025-09-11 17:06:07 +0800

    modifying

--- day4/main.go
+++ day4/main.go
@@ -8,6 +8,53 @@ import (
 	"time"
 )
 
+// define const
+const charX = "X"
+const charM = "M"
+const charA = "A"
+const charS = "S"
+const xmasLen = 3
+const masLen = 1
+
+func masCount_v2(matrix [][]string) int {
+	count := 0
+	matrixLen := len(matrix)
+
+	distances := [][]int{
+		{1, -1},  // leftup
+		{-1, -1}, // rightup
+		{1, 1},   // leftdown
+		{-1, 1},  // rightdown
+	}
+
+	for i := range matrixLen {
+		for j := range matrixLen {
+			if matrix[i][j] == charA {
+				continue
+			}
+
+			for _, distance := range distances {
+
+				if !(i < matrixLen-masLen && j < matrixLen-masLen && i >= masLen && j >= masLen) {
+					continue
+				}
+
+				x := distance[0]
+				y := distance[1]
+
+				current := matrix[i+y][i+x]
+				opposite := matrix[i-y][i-x]
+
+				if (current == charS && opposite == charM) || (current == charM && opposite == charS) {
+					count += 1
+				}
+
+			}
+		}
+	}
+
+	return count
+}
 func masCount(matrix [][]string) int {
 	count := 0
 	matrixLen := len(matrix)
@@ -60,155 +107,55 @@ func masCount(matrix [][]string) int {
 }
 
 func xmasCount_v2(matrix [][]string) int {
+
 	count := 0
 	matrixLen := len(matrix)
 
 	distances := [][]int{
-
-		// left
-		{1, 0},
-
-		// right
-		{-1, 0},
-
-		// down
-		{0, 1},
-
-		// up
-		{0, -1},
-
-		// leftup
-		{1, -1},
-
-		// rightup
-		{-1, -1},
-
-		// leftdown
-		{1, 1},
-
-		// rightdown
-		{-1, 1},
+		{1, 0},   // left
+		{-1, 0},  // right
+		{0, 1},   // down
+		{0, -1},  // up
+		{1, -1},  // leftup
+		{-1, -1}, // rightup
+		{1, 1},   // leftdown
+		{-1, 1},  // rightdown
 	}
 
-	fmt.Println(distances)
-	fmt.Println(distances[0][0])
-
-	for i := 0; i < matrixLen; i++ {
-		for j := 0; j < matrixLen; j++ {
-
+	for i := range matrixLen {
+		for j := range matrixLen {
 			for _, distance := range distances {
 
 				x := distance[0]
 				y := distance[1]
 
-				// stop if progressing
-				if x == 1 && j >= matrixLen-3 {
+				if x == 1 && j >= matrixLen-xmasLen {
 					continue
 				}
 
-				if y == 1 && i >= matrixLen-3 {
+				if y == 1 && i >= matrixLen-xmasLen {
 					continue
 				}
 
-				if x == -1 && j < 3 {
+				if x == -1 && j < xmasLen {
 					continue
 				}
 
-				if y == -1 && i < 3 {
+				if y == -1 && i < xmasLen {
 					continue
 				}
 
-				fmt.Println(matrixLen, i, j, x, y, distance)
+				// fmt.Println(matrixLen, i, j, x, y, distance)
 
-				if matrix[i][j] == "X" &&
-					matrix[i+1*y][j+1*x] == "M" &&
-					matrix[i+2*y][j+2*x] == "A" &&
-					matrix[i+3*y][j+3*x] == "S" {
+				if matrix[i][j] == charX &&
+					matrix[i+1*y][j+1*x] == charM &&
+					matrix[i+2*y][j+2*x] == charA &&
+					matrix[i+3*y][j+3*x] == charS {
 					count += 1
 				}
 
 			}
 
-			// left count
-			if j < matrixLen-3 {
-				if matrix[i][j] == "X" &&
-					matrix[i][j+1] == "M" &&
-					matrix[i][j+2] == "A" &&
-					matrix[i][j+3] == "S" {
-					count += 1
-				}
-			}
-
-			// right count
-			if j >= 3 {
-				if matrix[i][j] == "X" &&
-					matrix[i][j-1] == "M" &&
-					matrix[i][j-2] == "A" &&
-					matrix[i][j-3] == "S" {
-					count += 1
-				}
-			}
-
-			// down count
-			if i < matrixLen-3 {
-				if matrix[i][j] == "X" &&
-					matrix[i+1][j] == "M" &&
-					matrix[i+2][j] == "A" &&
-					matrix[i+3][j] == "S" {
-					count += 1
-				}
-
-			}
-			// up count
-			if i >= 3 {
-				if matrix[i][j] == "X" &&
-					matrix[i-1][j] == "M" &&
-					matrix[i-2][j] == "A" &&
-					matrix[i-3][j] == "S" {
-					count += 1
-				}
-			}
-
-			// left down
-			if i < matrixLen-3 && j < matrixLen-3 {
-				if matrix[i][j] == "X" &&
-					matrix[i+1][j+1] == "M" &&
-					matrix[i+2][j+2] == "A" &&
-					matrix[i+3][j+3] == "S" {
-					count += 1
-				}
-
-			}
-
-			// right down
-			if i < matrixLen-3 && j >= 3 {
-				if matrix[i][j] == "X" &&
-					matrix[i+1][j-1] == "M" &&
-					matrix[i+2][j-2] == "A" &&
-					matrix[i+3][j-3] == "S" {
-					count += 1
-				}
-			}
-
-			// left up
-			if i >= 3 && j < matrixLen-3 {
-				if matrix[i][j] == "X" &&
-					matrix[i-1][j+1] == "M" &&
-					matrix[i-2][j+2] == "A" &&
-					matrix[i-3][j+3] == "S" {
-					count += 1
-				}
-			}
-
-			// right up
-			if i >= 3 && j >= 3 {
-				if matrix[i][j] == "X" &&
-					matrix[i-1][j-1] == "M" &&
-					matrix[i-2][j-2] == "A" &&
-					matrix[i-3][j-3] == "S" {
-					count += 1
-				}
-			}
 		}
 	}
 	return count
@@ -329,7 +276,7 @@ func main() {
 	}
 
 	resultPartOne := xmasCount_v2(matrix)
-	resultPartTwo := masCount(matrix)
+	resultPartTwo := masCount_v2(matrix)
 
 	fmt.Println(resultPartOne)
 	fmt.Println(resultPartTwo)
