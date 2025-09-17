e66bcc2ba81c0a92435ce6bb1a0b385296355a3d
Peter <petervsjim@gmail.com>
2025-09-17 20:43:35 +0800

    awaiting for final modification -- struct!

--- day5/main.go
+++ day5/main.go
@@ -4,47 +4,56 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-func strSliToIntSli(strSli []string) ([]int, error) {
+// 1. for any error checkings, use %w insted of %v, explain in the solution
+// solution: %w operates like %v, but I provides alternate functionability like As Is and Unwrap
+//
+//	which gives some more ways to parse the error
+//
+// 2. change the naming (hint: use its core func to name)
+// solution: change strSli... to parseInts and renewUpdates to sortUpdates
+// 5. Idiomatic Go: capacity and memory allocation (space) issue
+// solved: use make to specify len and use i to assign var
+func parseInts(strSli []string) ([]int, error) {
 
-	var intSli []int
-	for _, str := range strSli {
+	intSli := make([]int, len(strSli))
+	for i, str := range strSli {
 		num, err := strconv.Atoi(str)
 		if err != nil {
-			return []int{0}, fmt.Errorf("error converting string to interger: %v", err)
+			return []int{0}, fmt.Errorf("error converting string to interger: %w", err)
 		}
-		intSli = append(intSli, num)
+		intSli[i] = num
 	}
 
 	return intSli, nil
 }
 
-func parseRuleParts(data string) (map[int][]int, [][]int, error) {
+func parseRuleParts(data string) (map[int]map[int]struct{}, [][]int, error) {
 
 	sections := strings.Split(data, "\n\n")
 
-	part1 := make(map[int][]int)
+	part1 := make(map[int]map[int]struct{})
 
 	for lines := range strings.FieldsSeq(sections[0]) {
 		strSep := strings.Split(lines, "|")
-		numSep, err := strSliToIntSli(strSep)
+		numSep, err := parseInts(strSep)
 		if err != nil {
-			return nil, nil, fmt.Errorf("\nerror parsing part 1 lines: %v", err)
+			return nil, nil, fmt.Errorf("\nerror parsing part 1 lines: %w", err)
 		}
 
 		first := numSep[0]
 		second := numSep[1]
 
-		val, ok := part1[first]
-		if ok && len(val) != 0 {
-			part1[first] = append(val, second)
+		_, ok := part1[first]
+		if ok {
+			part1[first][second] = struct{}{}
 		} else {
-			part1[first] = []int{second}
+			part1[first] = make(map[int]struct{})
+			part1[first][second] = struct{}{}
 		}
 	}
 
@@ -52,9 +61,9 @@ func parseRuleParts(data string) (map[int][]int, [][]int, error) {
 
 	for lines := range strings.FieldsSeq(sections[1]) {
 		strSep := strings.Split(lines, ",")
-		numSep, err := strSliToIntSli(strSep)
+		numSep, err := parseInts(strSep)
 		if err != nil {
-			return nil, nil, fmt.Errorf("\nerror parsing part 2 lines: %v", err)
+			return nil, nil, fmt.Errorf("\nerror parsing part 2 lines: %w", err)
 		}
 
 		part2 = append(part2, numSep)
@@ -63,13 +72,13 @@ func parseRuleParts(data string) (map[int][]int, [][]int, error) {
 	return part1, part2, nil
 }
 
-func checkUpdate(update []int, rules map[int][]int) (int, error) {
+// 3. Efficiency issue (hint: change the map[int][]int to map[int]map[int]bool or map[int]map[int]struct)
+// solved: modified data structure using map to do checking
+func checkUpdate(update []int, rules map[int]map[int]struct{}) (int, []int, error) {
 
 	if len(update)%2 == 0 {
-		return 0, fmt.Errorf("rule length should be odd")
+		return 0, nil, fmt.Errorf("rule length should be odd")
 	}
-
-	pass := true
 
 	for i := range update {
 		for j := range update {
@@ -78,42 +87,15 @@ func checkUpdate(update []int, rules map[int][]int) (int, error) {
 				continue
 			}
 
-			if !slices.Contains(rules[update[i]], update[j]) {
-				// fmt.Println("The pass did not pass since", update[j],
-				// 	"does not inside", update[i], rules[update[i]])
-
-				return 0, nil
+			_, ok := rules[update[i]][update[j]]
+			if !ok {
+				// returning malfunctioned update
+				return 0, update, nil
 			}
 		}
-
-		if !pass {
-			break
-		}
 	}
 
-	if pass {
-		// fmt.Println("The test has passed.", update[(len(update)-1)/2], "has been added.")
-		return update[(len(update)-1)/2], nil
-	}
-
-	return 0, nil
-
-}
-
-func checkUpdates(updates [][]int, rules map[int][]int) (int, error) {
-
-	result := 0
-
-	for _, update := range updates {
-		num, err := checkUpdate(update, rules)
-		if err != nil {
-			return 0, fmt.Errorf("error checking updates: %v", err)
-		}
-		result += num
-	}
-
-	return result, nil
-
+	return update[(len(update)-1)/2], nil, nil
 }
 
 func HasDuplicates(slice []int) bool {
@@ -127,69 +109,58 @@ func HasDuplicates(slice []int) bool {
 	return false // No duplicates found
 }
 
-func renewUpdates(updates [][]int, rules map[int][]int) (int, error) {
+// 4. duplication of the code
+// solved: modified main to make use of checkUpdate and its return value
+func sortUpdates(update []int, rules map[int]map[int]struct{}) (int, error) {
 
 	result := 0
 
-	for _, update := range updates {
+	var seqSlice [][]int
 
-		var seqSlice [][]int
+	for i := range update {
 
-		testNum, err := checkUpdate(update, rules)
-		if err != nil {
-			return 0, fmt.Errorf("error checking renew updates: %v", err)
-		}
-		if testNum != 0 {
-			// result += testNum
-			continue
-		}
+		count := 0
 
-		for i := range update {
+		for j := range update {
 
-			count := 0
-
-			for j := range update {
-
-				if i == j {
-					continue
-				}
-
-				if slices.Contains(rules[update[j]], update[i]) {
-					count += 1
-				}
+			if i == j {
+				continue
 			}
 
-			seqSlice = append(seqSlice, []int{update[i], count})
-		}
-		
-
-		sort.Slice(seqSlice, func(i, j int) bool {
-			return seqSlice[i][1] < seqSlice[j][1]
-		})
-
-		var sortedSlice []int
-		for _, num := range seqSlice {
-			sortedSlice = append(sortedSlice, num[0])
+			_, ok := rules[update[j]][update[i]]
+			if ok {
+				count += 1
+			}
 		}
 
-		// slice competency check: count must not duplicate
-		isDup := HasDuplicates(sortedSlice)
-		if isDup {
-			return 0, fmt.Errorf("malfunction sequence detected %v", err)
-		}
-
-		num, err := checkUpdate(sortedSlice, rules)
-		if err != nil {
-			return 0, fmt.Errorf("error checking updates: %v", err)
-		}
-
-		// fmt.Println(sortedSlice, result)
-
-		result += num
+		seqSlice = append(seqSlice, []int{update[i], count})
 	}
 
-	return result, nil
+	sort.Slice(seqSlice, func(i, j int) bool {
+		return seqSlice[i][1] < seqSlice[j][1]
+	})
 
+	var sortedSlice []int
+	for _, num := range seqSlice {
+		sortedSlice = append(sortedSlice, num[0])
+	}
+
+	// slice competency check: count must not duplicate
+	isDup := HasDuplicates(sortedSlice)
+	if isDup {
+		return 0, fmt.Errorf("malfunction sequence detected")
+	}
+
+	num, malUpdate, err := checkUpdate(sortedSlice, rules)
+	if err != nil {
+		return 0, fmt.Errorf("error checking updates %w", err)
+	}
+	if malUpdate != nil {
+		return 0, fmt.Errorf("malfunctioned update found")
+	}
+	result += num
+
+	return result, nil
 }
 
 func main() {
@@ -206,17 +177,27 @@ func main() {
 		log.Fatalf("\nerror parsing rule parts %v", err)
 	}
 
-	result1, err := checkUpdates(updates, rules)
-	if err != nil {
-		log.Fatalf("\nerror checking updates %v", err)
-	}
+	result1 := 0
+	result2 := 0
 
-	result2, err := renewUpdates(updates, rules)
-	if err != nil {
-		log.Fatalf("\nerror checking updates %v", err)
+	for _, update := range updates {
+
+		num, malUpdate, err := checkUpdate(update, rules)
+		if err != nil {
+			log.Fatalf("Error checking updates %v", err)
+		}
+		if num != 0 {
+			result1 += num
+		}
+		if malUpdate != nil {
+			malNum, err := sortUpdates(malUpdate, rules)
+			if err != nil {
+				log.Fatalf("Error sorting updates %v", err)
+			}
+			result2 += malNum
+		}
+
 	}
-	// fmt.Println(rules)
-	// fmt.Println(updates)
 
 	fmt.Println("result of part1:", result1)
 	fmt.Println("result of part2:", result2)
